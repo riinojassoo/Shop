96cdd1f3a2c3297b9c0a650a3b78c248e11986e0
riinojassoo <riin.ojassoo@hotmail.com>
2024-09-23 11:41:52 +0300

    SpaceshipIndex

---
+++ Shop/Shop/Controllers/SpaceshipsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Data;
+using Shop.Models.Spaceships;
+
+namespace Shop.Controllers
+{
+    public class SpaceshipsController : Controller
+    {
+        public readonly ShopContext _context;
+
+        public SpaceshipsController
+            (
+            ShopContext context
+            ) 
+        {
+            _context = context; 
+        }
+
+        public IActionResult Index()
+        {
+            var result = _context.Spaceships
+                .Select(x => new SpaceshipsIndexViewModel
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    Typename = x.Typename,
+                    BuiltDate = x.BuiltDate,
+                    Crew = x.Crew
+                });
+
+            return View(result);
+        }
+    }
+}
