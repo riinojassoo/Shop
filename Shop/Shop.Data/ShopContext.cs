4de4f975d6c293dd5460f4637f4f9b817614032c
riinojassoo <riin.ojassoo@hotmail.com>
2024-09-23 09:33:47 +0300

    SpaceshipSetup

---
+++ Shop/Shop.Data/ShopContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shop.Core.Domain;
+
+namespace Shop.Data
+{
+    public class ShopContext : DbContext
+    {
+        public ShopContext(DbContextOptions<ShopContext> options)
+        : base(options) { }
+
+
+        public DbSet<Spaceship> Spaceships { get; set; }
+
+
+    }
+}
