cb5c400bccf7a00b423ff0e6f50605ebdff7bdb1
riinojassoo <riin.ojassoo@hotmail.com>
2024-10-03 10:30:01 +0300

    KindergartenMigrationInit

--- Shop/Shop.Data/Migrations/20241003072447_KinderInit.Designer.cs
+++ Shop/Shop.Data/Migrations/20241003072447_KinderInit.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Shop.Data;
 
@@ -11,9 +12,11 @@ using Shop.Data;
 namespace Shop.Data.Migrations
 {
     [DbContext(typeof(ShopContext))]
-    partial class ShopContextModelSnapshot : ModelSnapshot
+    [Migration("20241003072447_KinderInit")]
+    partial class KinderInit
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
