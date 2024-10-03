4c9ef41bea3d082845fbbe51659e00c34d04fdec
riinojassoo <riin.ojassoo@hotmail.com>
2024-10-03 10:00:19 +0300

    KindergartenInit

--- Shop/Shop.Data/ShopContext.cs
+++ Shop/Shop.Data/ShopContext.cs
@@ -11,7 +11,7 @@ namespace Shop.Data
 
         public DbSet<Spaceship> Spaceships { get; set; }
         public DbSet<FileToApi> FileToApis { get; set; }
-
+        public DbSet<Kindergarten> Kindergartens { get; set; }
 
     }
 }
---
+++ Shop/Shop/Controllers/KindergartenController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Shop.Controllers
+{
+    public class KindergartenController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
