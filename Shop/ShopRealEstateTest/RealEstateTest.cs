221a5aa0bf4cf8632ccdded15ec774221d799132
riinojassoo <riin.ojassoo@hotmail.com>
2024-10-21 11:39:31 +0300

    RealEstateTestsTwoAndThree

--- Shop/ShopRealEstateTest/RealEstateTest.cs
+++ Shop/ShopRealEstateTest/RealEstateTest.cs
@@ -30,14 +30,26 @@ namespace ShopRealEstateTest
         public async Task ShouldNot_GetByIdRealEstate_WhenReturnsNotEqual()
         {
             //Arrange
-            RealEstateDto dto = new();
-
+            Guid wrongGuid = Guid.Parse(Guid.NewGuid().ToString());
+            Guid guid = Guid.Parse("6e07475d-9ca5-4449-96cd-3bbb9de5bc81");
 
             //Act
-
+            await Svc<IRealEstateServices>().GetAsync(guid);
 
             //Assert
+            Assert.NotEqual(wrongGuid, guid);
 
-        }
+		}
+
+        [Fact]
+        public async Task Should_GetByIdRealEstate_WhenReturnsEqual()
+        {
+            Guid databaseGuid = Guid.Parse("89c057b7-918c-4296-992a-0938a16e3d1d");
+            Guid guid = Guid.Parse("89c057b7-918c-4296-992a-0938a16e3d1d");
+
+			await Svc<IRealEstateServices>().GetAsync(guid);
+
+            Assert.Equal(databaseGuid, guid);
+		}
     }
 }
