fb3d4b12f56aa80934a46f361829bde9762cea3d
riinojassoo <riin.ojassoo@hotmail.com>
2024-10-23 16:20:14 +0300

    KindergartenTestPracticeTest

--- Shop/Shop.KindergartenTest/KindergartenTest.cs
+++ Shop/Shop.KindergartenTest/KindergartenTest.cs
@@ -6,6 +6,19 @@ namespace Shop.KindergartenTest
 {
 	public class KindergartenTest : TestBase
 	{
-		
+		[Fact]
+		public async Task ShouldNot_GetByIdKindergarten_WhenReturnsNotEqual()
+		{
+			//Arrange
+			Guid wrongGuid = Guid.Parse(Guid.NewGuid().ToString());
+			Guid guid = Guid.Parse("75063866-1bd2-4cff-9abb-3217e6fc7c02");
+
+			//Act
+			await Svc<IKindergartenServices>().DetailAsync(guid);
+
+			//Assert
+			Assert.NotEqual(wrongGuid, guid);
+
+		}
 	}
 }
