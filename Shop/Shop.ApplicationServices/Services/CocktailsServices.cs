74701ba1bf3bc5244525c77bc5e97778becd23d7
riinojassoo <riin.ojassoo@hotmail.com>
2024-11-18 11:31:40 +0200

    CocktailsSetup

---
+++ Shop/Shop.ApplicationServices/Services/CocktailsServices.cs
@@ -0,0 +1,29 @@
+﻿using Nancy.Json;
+using Shop.Core.Dto.Cocktails;
+using Shop.Core.Dto.WeatherDtos.AccuWeatherDtos;
+using Shop.Core.ServiceInterface;
+using System.Collections.Generic;
+using System.Net;
+
+namespace Shop.ApplicationServices.Services
+{
+    public class CocktailsServices : ICocktailsServices
+    {
+        public async Task<List<CocktailsDto>> CocktailsResult()
+        {
+            string url = $"www.thecocktaildb.com/api/json/v1/1/search.php?s={dto.strDrink}";
+            List<CocktailsDto> cocktailsList = new List<CocktailsDto>();
+
+            using (WebClient client = new WebClient())
+            {
+                string json = client.DownloadString(url);
+
+                List<CocktailsDto> coctailsResult = new JavaScriptSerializer().Deserialize<List<CocktailsDto>>(json);
+
+                dto.strDrink = coctailsResult[0].strDrink;
+            }
+
+    }
+}
+
+
---
+++ Shop/Shop.Core/ServiceInterface/ICocktailsServices.cs
@@ -0,0 +1,9 @@
+﻿using Shop.Core.Dto.Cocktails;
+
+namespace Shop.Core.ServiceInterface
+{
+    public interface ICocktailsServices
+    {
+        Task<List<CocktailsDto>> CocktailsResult();
+    }
+}
