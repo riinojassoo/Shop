5c9ccdcca5e47a9e49c58942de0478a60b66fe98
riinojassoo <riin.ojassoo@hotmail.com>
2024-11-21 15:41:34 +0200

    OpenWeathersController

---
+++ Shop/Shop/Controllers/OpenWeathersController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Core.ServiceInterface;
+
+namespace Shop.Controllers
+{
+    public class OpenWeathersController : Controller
+    {
+        private readonly IOpenWeatherServices _openWeatherServices;
+
+        public OpenWeathersController
+            (
+            IOpenWeatherServices openWeatherServices
+            )
+        {
+            _openWeatherServices = openWeatherServices;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
