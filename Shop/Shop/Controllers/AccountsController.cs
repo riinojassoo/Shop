90a937169decf10fbdbe5781a571a5e03f41add5
riinojassoo <riin.ojassoo@hotmail.com>
2024-12-09 10:06:42 +0200

    LoginHttpPost

--- Shop/Shop/Controllers/AccountsController.cs
+++ Shop/Shop/Controllers/AccountsController.cs
@@ -83,5 +83,45 @@ namespace Shop.Controllers
 
 			return View(vm);
 		}
+
+		[HttpPost]
+		[AllowAnonymous]
+		public async Task<IActionResult> Login(LoginViewModel model, string? returnUrl)
+		{
+			model.ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+
+			if (ModelState.IsValid)
+			{
+				var user = await _userManager.FindByEmailAsync(model.Email);
+
+				if (user != null && !user.EmailConfirmed && (await _userManager.CheckPasswordAsync(user, model.Password)))
+				{
+					ModelState.AddModelError(string.Empty, "Email not confirmed yet");
+					return View(model);
+				}
+
+				var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, true);
+
+				if (result.Succeeded)
+				{
+					if (!string.IsNullOrEmpty(returnUrl) && Url.IsLocalUrl(returnUrl))
+					{
+						return Redirect(returnUrl);
+					}
+					else
+					{
+						return RedirectToAction("Index", "Home");
+					}
+				}
+
+				if (result.IsLockedOut)
+				{
+					return View("AccountLocked");
+				}
+				ModelState.AddModelError("", "Invalid Login Attempt");
+			}
+
+			return View(model);
+		}
 	}
 }
