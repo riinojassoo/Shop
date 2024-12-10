0c1aafcc38218e9afaac1d238c2b09958cbcea4d
riinojassoo <riin.ojassoo@hotmail.com>
2024-12-10 16:46:54 +0200

    ShopChangePasswordDropdown

--- Shop/Shop/Controllers/AccountsController.cs
+++ Shop/Shop/Controllers/AccountsController.cs
@@ -162,5 +162,42 @@ namespace Shop.Controllers
             }
 			return View(model);
         }
+
+		[HttpGet]
+		public IActionResult ChangePassword()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> ChangePassword(ChangePasswordViewModel model)
+		{
+			if (ModelState.IsValid)
+			{
+				var user = await _userManager.GetUserAsync(User);
+
+				if (user == null)
+				{
+					return RedirectToAction("Login");
+				}
+
+				var result = await _userManager.ChangePasswordAsync(user, model.CurrentPassword, model.NewPassword);
+
+				if (!result.Succeeded)
+				{
+					foreach (var error in result.Errors)
+					{
+						ModelState.AddModelError(string.Empty, error.Description);
+					}
+
+					return View();
+				}
+
+				await _signInManager.RefreshSignInAsync(user);
+				return View("ChangePasswordConfirmation");
+			}
+
+			return View(model);
+		}
     }
 }
---
+++ Shop/Shop/Models/Accounts/ChangePasswordViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shop.Models.Accounts
+{
+    public class ChangePasswordViewModel
+    {
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Current password")]
+        public string CurrentPassword { get; set; } = string.Empty;
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "New Password")]
+        public string NewPassword { get; set; } = string.Empty;
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm new password")]
+        [Compare("NewPassword", ErrorMessage = "The new password and confirmationpassword do not match.")]
+        public string ConfirmPassword { get; set; } = string.Empty;
+    }
+}
